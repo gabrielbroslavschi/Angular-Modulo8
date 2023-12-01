cc521843ed869fb5cea77fa5812aa2e67fde81dd
Gabriel Broslavschi <gabrielbroslavschidacosta@gmail.com>
2023-12-01 13:58:56 -0300

    Upload de Arquivo: Request com FormData~

--- src/app/cursos/upload-file/upload-file/upload-file.component.ts
+++ src/app/cursos/upload-file/upload-file/upload-file.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { UploadFileService } from '../upload-file.service';
 
 @Component({
   selector: 'app-upload-file',
   templateUrl: './upload-file.component.html',
-  styleUrls: ['./upload-file.component.css']
+  styleUrls: ['./upload-file.component.css'],
 })
 export class UploadFileComponent {
+  files!: Set<File>;
 
-  constructor(){}
+  constructor(private service: UploadFileService) {}
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
@@ -17,12 +19,22 @@ export class UploadFileComponent {
 
   onChange(event: any) {
     const selectedFiles = <FileList>event?.srcElement?.files;
-  
-    if (selectedFiles && selectedFiles.length > 0) {
-      // console.log(selectedFiles[0].name);
-      document.getElementById("customFileLabel")!.innerHTML = selectedFiles[0].name;
-    } else {
-      console.warn('Nenhum arquivo selecionado.');
+
+    const fileNames = [];
+    this.files = new Set()
+
+    for (let i = 0; i < selectedFiles.length; i++) {
+      fileNames.push(selectedFiles[i].name);
+      this.files.add(selectedFiles[i]);
+    }
+
+    document.getElementById('customFileLabel')!.innerHTML =
+      fileNames.join(', ');
+  }
+
+  onUplad() {
+    if(this.files && this.files.size > 0){
+      this.service.upload(this.files, "http://localhost:8000/upload").subscribe(res => console.log("Upload Concluído"));
     }
   }
 }
